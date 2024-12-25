3b004e888112998ae9538e159737410b8de50fed
anshcodez <ytanshu20@gmail.com>
2024-12-25 13:48:45 +0530

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-BOT_TOKEN = os.getenv("BOT_TOKEN", "7916798453:AAGsAqqePGz5pqXqAFmmSu40tXfhG-7GHc8")
-API_URL = "https://turecaller.pikaapis0.workers.dev/?number={}"
-TELEGRAM_API_URL = f"https://api.telegram.org/bot{BOT_TOKEN}"
