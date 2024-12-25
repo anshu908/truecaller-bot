74da0efecd08ec68a571599178eed1ce2d661e44
anshcodez <ytanshu20@gmail.com>
2024-12-25 13:51:19 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -3,7 +3,7 @@ from os import path, getenv
 class Config:
     API_ID = int(getenv("API_ID", "0112234"))
     API_HASH = getenv("API_HASH", "abcdefg")
-    BOT_TOKEN = getenv("BOT_TOKEN", "1234567891:AdDfgFRFVVfDEhdhyjjvjjftSEW")
+    BOT_TOKEN = getenv("BOT_TOKEN", "7916798453:AAGsAqqePGz5pqXqAFmmSu40tXfhG-7GHc8")
     CHID = int(getenv("CHID", "-1000112234"))
     SUDO = list(map(int, getenv("SUDO").split()))
     MONGO_URI = getenv("MONGO_URI", "")
